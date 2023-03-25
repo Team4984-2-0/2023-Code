9b6320c175723ad1d0a20c246bb6b0404ca1e020
Melvin <melvin.dettinger@gmail.com>
2023-03-24 21:49:57 -0700

    Update AutonomousCommand4_copy.java

--- src/main/java/frc/robot/commands/AutonomousCommand4_copy.java
+++ src/main/java/frc/robot/commands/AutonomousCommand4_copy.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import java.util.concurrent.TimeUnit;
 
 import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Grabber;
@@ -20,8 +21,7 @@ public class AutonomousCommand4_copy extends CommandBase {
     private PIDDriveTrain m_DriveTrain;
     private int sleepCounter;
     private int sleepCounterMax;
-    private double NAVXLevel;
-    private double count = 0;
+    private Timer timer = new Timer();
     private boolean end = false;
 
 
@@ -29,14 +29,14 @@ public class AutonomousCommand4_copy extends CommandBase {
         m_Grabber = Grabber_sub;
         m_Winch = Winch_sub;
         m_DriveTrain = DriveTrain_sub;
-        sleepCounter = 0;
-        sleepCounterMax = 80;
+        //sleepCounter = 0;
+        //sleepCounterMax = 80;
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        NAVXLevel = m_DriveTrain.getNavXRoll();
+        timer.start(); //start timer ~ auto period
         m_DriveTrain.setBrakeMode();
     }
 
@@ -53,32 +53,39 @@ public class AutonomousCommand4_copy extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        int count = 0; //variable determines which while loop is execited
+        double NAVXLevel = m_DriveTrain.getNavXRoll();
+
         if (RobotState.isAutonomous()) {
             
+            //while loop controls servo open, grabber, and reverse until navX sees not flat
             while ((Math.abs(m_DriveTrain.getNavXRoll() - NAVXLevel) < 1.5) && count == 0){
-                sleepCounter = sleepCounter + 1;
 
-                if (sleepCounter == 1) {
+                if (timer.hasElapsed(1)) {
+                    //releases grabber
                     m_Winch.moveservo180();
-                    if(RobotState.isTeleop()) {
+                    if(RobotState.isTeleop() || timer.get() >= 15) {
                         break;
                     }
                 }
-                else if (sleepCounter == 45){
+                else if (timer.hasElapsed(1.5)){
+                    //spits out game piece
                     m_Grabber.open();
-                    if(RobotState.isTeleop()) {
+                    if(RobotState.isTeleop() || timer.get() >= 15) {
                         break;
                     }
                 }
-                else if (sleepCounter == 76){
+                else if (timer.hasElapsed(2.5)){
+                    //stops grabber wheels
                     m_Grabber.stop();
-                    if(RobotState.isTeleop()) {
+                    if(RobotState.isTeleop() || timer.get() >= 15) {
                         break;
                     }
                 }
-                else if (sleepCounter > 76){
+                else if (timer.get() > 2.5){
+                    //reverses robot
                     m_DriveTrain.drive(-0.45,0.45);
-                    if(RobotState.isTeleop()) {
+                    if(RobotState.isTeleop() || timer.get() >= 15) {
                         break;
                     }
                 }
@@ -86,21 +93,25 @@ public class AutonomousCommand4_copy extends CommandBase {
 
             count ++;
 
+            //while loop controls balance once robot has reversed onto charge station
             while ((Math.abs(m_DriveTrain.getNavXRoll() - NAVXLevel) > 1.5) && count != 0){
                 
-                if((m_DriveTrain.getNavXRoll()) < NAVXLevel){
+                if(m_DriveTrain.getNavXRoll() < NAVXLevel){
+                    //reverses robot if robot is pitching down relative to front of robot
                     m_DriveTrain.drive(-0.45,0.45);
-                    System.out.println("Going Down");
+                    //System.out.println("Going Down");
                 }
-                else if((m_DriveTrain.getNavXRoll()) > NAVXLevel){
+                else if(m_DriveTrain.getNavXRoll() > NAVXLevel){
+                    //moves robot forward if robot is pitching up relative to front of robot
                     m_DriveTrain.drive(0.45,-0.45);
-                    System.out.println("Going Up");
+                    //System.out.println("Going Up");
                 }
-                if(RobotState.isTeleop()) {
+                if(RobotState.isTeleop() || timer.get() >= 15) {
                     break;
                 }
             }
 
+            //sets end settings for robot and ends auto command
             if ((Math.abs(m_DriveTrain.getNavXRoll() - NAVXLevel) < 1.5) && count != 0){
                 m_DriveTrain.drive(0, 0);
                 m_DriveTrain.setCoastMode();
@@ -112,18 +123,20 @@ public class AutonomousCommand4_copy extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        timer.stop();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (end == true || RobotState.isTeleop())
+        if (end == true || RobotState.isTeleop() || timer.get() >= 15)
         {
             if(RobotState.isTeleop()) {
                 System.out.println("Teleop has started fiinishing Autonomous");
             }
             System.out.println("Autonomous Finished");
-            sleepCounter = 0;
+            //sleepCounter = 0;
+            timer.stop();
             return true;
         }
         else 
